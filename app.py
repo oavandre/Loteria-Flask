6714fb44c675768f73a3815e733728a2ad7cfedb
Osmar <oav.andre@gmail.com>
2020-08-27 19:20:54 -0300

    estrutura completa

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, send_from_directory
+import pickle
 import os
-from flask import Flask
-
 
 app = Flask(__name__)
+model = pickle.load(open('model_mega2.pkl', 'rb'))
 
 
-@app.route("/")
-def index():
-    return "<h1>Hello World</hi>"
+@app.route('/favicon.')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    numeros_desenas = [int(x) for x in request.form.values()]
+    final_prev = [np.array(numeros_desenas)]
+    resultado = model.predict(final_prev)
+
+    valida_duplicado= len(numeros_desenas) != len(set(numeros_desenas))
+
+
+    if valida_duplicado :
+        return render_template('index.html', mensagem_probabilidade='Não pode repetir Numero')
+    else:
+        return render_template('index.html', mensagem_probabilidade='Probabilidade de ganhar:  {} %'.format(resultado))
+
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
